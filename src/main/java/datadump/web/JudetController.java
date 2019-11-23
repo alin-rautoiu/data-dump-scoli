f48c91ecb6959b4db84a88dd2a525b175635f987
Alin Răuțoiu <alin.rautoiu@gmail.com>
2019-11-23 16:19:53 +0200

    Exclus _id din query

--- src/main/java/datadump/web/JudetController.java
+++ src/main/java/datadump/web/JudetController.java
@@ -6,6 +6,7 @@ import com.mongodb.DBObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Projections;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -29,7 +30,7 @@ public class JudetController {
 
         List<Map<String, Object>> values = new LinkedList<>();
 
-        MongoCursor cursor = collection.find().cursor();
+        MongoCursor cursor = collection.find().projection(Projections.excludeId()).cursor();
         try {
             while (cursor.hasNext()) {
                 values.add((Map<String, Object>) cursor.next());
