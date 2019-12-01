92b5f88dff39d69caf103fdeb3718b26c21583e8
Alin Răuțoiu <alin.rautoiu@gmail.com>
2019-12-01 17:26:12 +0200

    Eliminat upload-dir, incarc direct in radacina

--- src/main/java/datadump/common/StorageProperties.java
+++ src/main/java/datadump/common/StorageProperties.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 @ConfigurationProperties("services")
 public class StorageProperties {
-    private String location = JavaCreateDirectory.createDirectory("upload-dir");
+    private String location = JavaCreateDirectory.createDirectory("");
 
     public StorageProperties() throws IOException {
     }
